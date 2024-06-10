3195d5bf065531ca3bdd9c2790483a698ee75bb6
Elkadyy2001 <144770150+Elkadyy2001@users.noreply.github.com>
2024-06-10 10:58:10 +0300

    Add files via upload

---
+++ FastlaneScreenShot/FastlaneScreenShot/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FastlaneScreenShot
+//
+//  Created by Ibrahim Elkady on 10/06/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
